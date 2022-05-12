8475f930493de5fd3dc1011a1614e949f0cd38a8
MurabitoF <francomurabito@outlook.com>
2022-05-12 19:07:08 -0300

    About me page init

--- src/pages/AboutMe/index.js
+++ src/pages/AboutMe/index.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import './index.scss'
 
 const AboutMe = () => {
   return (
-    <div>AboutMe</div>
+    <div className='about-me'>
+      <div className='about-me-photo'>
+        <img src='/IMG_Franco3.jpg' alt='Franco Murabito' />
+      </div>
+      <div className='about-me-info'>
+        <h2 className='text-lg text-hover-effect'>Who i am?</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Viverra accumsan in nisl nisi scelerisque eu ultrices. Eu volutpat odio facilisis mauris sit amet massa vitae tortor. Malesuada pellentesque elit eget gravida cum sociis natoque. Diam maecenas ultricies mi eget mauris pharetra. Sed nisi lacus sed viverra tellus in hac. Vel quam elementum pulvinar etiam non quam lacus suspendisse faucibus. Nisl pretium fusce id velit ut. Diam maecenas ultricies mi eget mauris. Lorem sed risus ultricies tristique nulla aliquet enim.
+        </p>
+      </div>
+    </div>
   )
 }
 
