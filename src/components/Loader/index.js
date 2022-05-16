4a32401cc66cba703a31638310024683574cdeeb
MurabitoF <francomurabito@outlook.com>
2022-05-16 15:39:20 -0300

    Loader added to projects page

---
+++ src/components/Loader/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './index.scss'
+
+const Loader = () => {
+  return (
+    <div className="sk-bounce sk-center">
+        <div className="sk-bounce-dot pink"></div>
+        <div className="sk-bounce-dot cyan"></div>
+    </div>
+  )
+}
+
+export default Loader
--- src/pages/Projects/index.js
+++ src/pages/Projects/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { FaGithub } from 'react-icons/fa'
 import Card from '../../components/Card'
+import Loader from '../../components/Loader'
 import useProjects from '../../hooks/useProjects'
 import './index.scss'
 
@@ -10,11 +11,15 @@ const Projects = () => {
   return (
     <section className='projects'>
       <h2 className='text-lg'>Here are some of the projects I have been working on</h2>
+      {!repos.length
+        ? <Loader />
+        : (
       <div className='projects-grid-container'>
         {repos.map(repo => {
           return <Card key={repo.title} {...repo} />
         })}
       </div>
+          )}
       <h3>This are some of my projects if you want to see more please check my github profile</h3>
       <a href='https://github.com/MurabitoF'>
         <FaGithub className='svg-hover-effect' color='#EFEFEF' size={'4em'} />
